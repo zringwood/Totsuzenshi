ff3d2f899739b4f9612ecdf7debd91a7ba6f0548
Zachary Ringwood <zringwood@gmail.com>
2025-11-10 03:33:50 -0500

    added drill page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        pathMatch:"full",
+        loadComponent: () => {
+            return import("./home/home").then((m) => m.Home)
+        }
+    }, 
+    {
+        path:"drill", 
+        loadComponent: () => {
+            return import("./drill/drill").then((m) => m.Drill)
+        }
+    }
+];
